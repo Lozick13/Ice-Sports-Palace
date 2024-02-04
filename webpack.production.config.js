462e2a8dc28b36415e423283f5c505f5529a1e7d
Dmitriy Lozichnyy <dlozichnyy2005@gmail.com>
2024-02-04 15:41:03 +0700

    webpack setup

---
+++ webpack.production.config.js
@@ -0,0 +1,22 @@
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+
+module.exports = {
+	mode: 'production',
+	optimization: {
+		minimizer: [
+			new ImageMinimizerPlugin({
+				minimizer: {
+					implementation: ImageMinimizerPlugin.imageminMinify,
+					options: {
+						plugins: [
+							['gifsicle', { interlaced: true }],
+							['jpegtran', { progressive: true }],
+							['optipng', { optimizationLevel: 5 }],
+							['svgo', { name: 'preset-default' }],
+						],
+					},
+				},
+			}),
+		],
+	},
+}
