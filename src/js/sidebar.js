d4a40118a2314290e2662de9e5c4ecee9436b9e0
Dmitriy Lozichnyy <dlozichnyy2005@gmail.com>
2024-02-10 23:34:15 +0700

    price, services

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,17 +1,21 @@
 export function sidebarOpen() {
   const sidebarBtn = document.querySelector('#sidebar-open');
   const sidebar = document.querySelector('.sidebar');
+  const body = document.querySelector('body');
 
   sidebarBtn.addEventListener('click', () => {
     sidebar.classList.add('active');
+    body.classList.add('scroll-bar-off');
   });
 }
 
 export function sidebarClose() {
   const sidebarBtn = document.querySelector('#sidebar-close');
   const sidebar = document.querySelector('.sidebar');
+  const body = document.querySelector('body');
 
   sidebarBtn.addEventListener('click', () => {
     sidebar.classList.remove('active');
+    body.classList.remove('scroll-bar-off');
   });
 }
